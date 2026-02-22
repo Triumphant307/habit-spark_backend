0df90c795148bc8abef1d0cef66ad63c93171811
Triumphant307 <usuangbontriumphant@gmail.com>
2026-02-22 23:56:50 +0100

    feat: Add global error handling and 404 not found middleware

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -5,7 +5,7 @@ export const errorHandler = (err, req, res, next) => {
   const message = err.message || "Internal Server Error";
 
   res.status(statusCode).json({
-    status: "error",
+    status: err.status || (statusCode >= 500 ? "fail" : "error"),
     statusCode,
     message,
     ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
